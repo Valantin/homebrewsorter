fdc35ae8ae4907a4688404ce475b3e28506323c7
valantin89 <valantin89@884f289f-8310-f739-b498-11dc9548fc3b>
2010-10-23 14:08:58 +0000

    add fix to move operation(tested) add copy operation (untested, for a future filebrowser)

--- trunk/src/dirOperation.c
+++ trunk/src/dirOperation.c
@@ -1,9 +1,24 @@
-#include <pspiofilemgr.h>
-#include <string.h>
-#include <malloc.h>
-#include <memory.h>
+/*
+    PSP HomeBrewSorter GUI
+        Copyright (C) valantin <valantin89@gmail.com>
 
-void sceIoMove(char *oldPath,char *newPath) { 
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "dirOperation.h"
+
+void sceIoMove(char *oldPath,char *newPath) {
     SceUID oldFile; 
     SceUID newFile;    
     int readSize; 
@@ -18,31 +33,31 @@ void sceIoMove(char *oldPath,char *newPath) {
     sceIoRemove(oldPath);	
 }
 
-sceIoMvdir(char *oldPath, char *newPath){
-    sceIoMkdir(destpath, 0777);
+void sceIoMvdir(char *oldPath, char *newPath){
+    sceIoMkdir(newPath, 0777);
     char *fullOldPath;
     char *fullNewPath;
     SceIoDirent oneDir;
-    int oDir = sceIoDopen(path);
+    int oDir = sceIoDopen(oldPath);
     if (oDir < 0){
         return;
     }
-	while (1){
+    while (1){
         memset(&oneDir, 0, sizeof(SceIoDirent));
         if (sceIoDread(oDir, &oneDir) <= 0)
             break;
         if (!strcmp(oneDir.d_name, ".") || !strcmp(oneDir.d_name, ".."))
             continue;
-        if (path[strlen(oldPath)-1] != '/'){
+        if (oldPath[strlen(oldPath)-1] != '/'){
             fullOldPath = (char *)calloc(strlen(oldPath)+strlen(oneDir.d_name)+2,sizeof(char));
             fullNewPath = (char *)calloc(strlen(newPath)+strlen(oneDir.d_name)+2,sizeof(char));
-            sprintf(fullOldPath,"%/%",oldPath,oneDir.d_name);
-            sprintf(fullNewPath,"%/%",newPath,oneDir.d_name);
+            sprintf(fullOldPath,"%s/%s",oldPath,oneDir.d_name);
+            sprintf(fullNewPath,"%s/%s",newPath,oneDir.d_name);
         } else {
             fullOldPath = (char *)calloc(strlen(oldPath)+strlen(oneDir.d_name)+1,sizeof(char));
             fullNewPath = (char *)calloc(strlen(newPath)+strlen(oneDir.d_name)+1,sizeof(char));
-            sprintf(fullOldPath,"%%",oldPath,oneDir.d_name);
-            sprintf(fullNewPath,"%%",newPath,oneDir.d_name);
+            sprintf(fullOldPath,"%s%s",oldPath,oneDir.d_name);
+            sprintf(fullNewPath,"%s%s",newPath,oneDir.d_name);
         }
         if (FIO_S_ISDIR(oneDir.d_stat.st_mode)){
             sceIoMvdir(fullOldPath,fullNewPath);
@@ -52,6 +67,57 @@ sceIoMvdir(char *oldPath, char *newPath){
         free(fullOldPath);
         free(fullNewPath);
     }
-	sceIoDclose(oDir);
-	sceIoRmdir(path);
+    sceIoDclose(oDir);
+    sceIoRmdir(oldPath);
+}
+
+void sceIoCopy(char *oldPath,char *newPath) {
+    SceUID oldFile; 
+    SceUID newFile;    
+    int readSize; 
+    char filebuf[0x8000]; 
+    oldFile = sceIoOpen(oldPath, PSP_O_RDONLY, 0777); 
+    newFile = sceIoOpen(newPath, PSP_O_WRONLY | PSP_O_CREAT | PSP_O_TRUNC, 0777); 
+    while ((readSize = sceIoRead(oldFile, filebuf, 0x08000)) > 0) { 
+        sceIoWrite(newFile, filebuf, readSize);
+    }
+    sceIoClose(newFile);
+    sceIoClose(oldFile);	
+}
+
+void sceIoCpdir(char *oldPath, char *newPath){
+    sceIoMkdir(newPath, 0777);
+    char *fullOldPath;
+    char *fullNewPath;
+    SceIoDirent oneDir;
+    int oDir = sceIoDopen(oldPath);
+    if (oDir < 0){
+        return;
+    }
+    while (1){
+        memset(&oneDir, 0, sizeof(SceIoDirent));
+        if (sceIoDread(oDir, &oneDir) <= 0)
+            break;
+        if (!strcmp(oneDir.d_name, ".") || !strcmp(oneDir.d_name, ".."))
+            continue;
+        if (oldPath[strlen(oldPath)-1] != '/'){
+            fullOldPath = (char *)calloc(strlen(oldPath)+strlen(oneDir.d_name)+2,sizeof(char));
+            fullNewPath = (char *)calloc(strlen(newPath)+strlen(oneDir.d_name)+2,sizeof(char));
+            sprintf(fullOldPath,"%s/%s",oldPath,oneDir.d_name);
+            sprintf(fullNewPath,"%s/%s",newPath,oneDir.d_name);
+        } else {
+            fullOldPath = (char *)calloc(strlen(oldPath)+strlen(oneDir.d_name)+1,sizeof(char));
+            fullNewPath = (char *)calloc(strlen(newPath)+strlen(oneDir.d_name)+1,sizeof(char));
+            sprintf(fullOldPath,"%s%s",oldPath,oneDir.d_name);
+            sprintf(fullNewPath,"%s%s",newPath,oneDir.d_name);
+        }
+        if (FIO_S_ISDIR(oneDir.d_stat.st_mode)){
+            sceIoMvdir(fullOldPath,fullNewPath);
+        } else if(FIO_S_ISREG(oneDir.d_stat.st_mode)){
+            sceIoMove(fullOldPath,fullNewPath);
+        }
+        free(fullOldPath);
+        free(fullNewPath);
+    }
+    sceIoDclose(oDir);
 }
